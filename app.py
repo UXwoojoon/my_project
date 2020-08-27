d380af0dfa325586ed9b2e3705f0dde85f89b1e4
UXwoojoon <67641391+UXwoojoon@users.noreply.github.com>
2020-08-27 09:12:14 +0900

    -08.26
    -08.26

--- app.py
+++ app.py
@@ -15,12 +15,21 @@ def home():
 
 # 뉴스, 날씨, 쇼핑, 경제 웹크롤링 api
 # 뉴스
-@app.route('news', methods=['GET'])
-def news():
+@app.route('/headline', methods=['GET'])
+def headline():
     html = requests.get('https://news.naver.com/')
     soup = BeautifulSoup(html.text, 'html.parser')
 
-    data1 = soup.find('div', {'id': 'today_main_news'})
+    data = soup.find('div', {'id': 'today_main_news'})
+    headline_title = data.select_one('div[class=hdline_flick_item] > a > div > p').text
+    headline_img = data.select_one('div[class=hdline_flick_item] > a > img').get('src')
+    headline_list = data.select('ul > li')
+    hl_array = []
+    for list in headline_list:
+        hl_array.append(list.select_one('div[class=hdline_article_tit] > a').text)
+
+    return jsonify({'헤드라인 이미지': headline_img, '헤드라인 타이틀': headline_title, '기사들': hl_array})
+
 
 
 # 날씨
@@ -45,4 +54,46 @@ def weather():
 # 쇼핑
 @app.route('/shopping', methods=['GET'])
 def shopping():
-    
+    html = requests.get('https://search.shopping.naver.com/best100v2/main.nhn')
+    soup = BeautifulSoup(html.text, 'html.parser')
+
+    data1 = soup.select('ul[id=popular_srch_lst] > li')
+    topten = []
+    for line in data1:
+        rank = line.select_one('em').text
+        name = line.select_one('span[class=txt]').text
+        variation = line.select_one('span[class=vary]').text
+        tt_element = {'순위': rank, '이름': name, '변동': variation}
+        topten.append(tt_element)
+
+    return jsonify({'10위': topten, 'msg': 'success'})
+
+
+
+
+#금융
+@app.route('/finance', methods=['GET'])
+def finance():
+    html = requests.get('https://finance.naver.com/')
+    soup = BeautifulSoup(html.text, 'html.parser')
+
+    #kospi = soup.select_one('div[class=section_stock] > div:nth-child(1) > div > ').text
+    #kosdaq = soup.select_one('div[class=section_stock] > div:nth-child(2) > a > span > span[class=num]').text
+
+    top_traded = soup.select('tbody[id=_topItems1] > tr')
+    top_price = soup.select('tbody[id=_topItems4] > tr')
+    toptraded_array = []
+    topprice_array = []
+    for table in top_traded:
+        toptraded_array.append(table.select_one('th > a').text)
+    for table in top_price:
+        topprice_array.append(table.select_one('th > a').text)
+
+    return jsonify({'거래상위': toptraded_array, '시가총액': topprice_array})
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',port=5000,debug=True)
+
